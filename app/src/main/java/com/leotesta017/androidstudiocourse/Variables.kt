302153a0d2771992fff66de0926bcd42883440ab
Leonardo Zamudio Gómez <leoleozamudiogomez@gmail.com>
2024-08-07 05:31:12 -0600

    VariablesPart2

--- app/src/main/java/com/leotesta017/androidstudiocourse/Variables.kt
+++ app/src/main/java/com/leotesta017/androidstudiocourse/Variables.kt
@@ -11,18 +11,34 @@ fun main()
 
     // Variable num INT (-2, 147, 483, 647 a 2, 147, 483, 647)
     val age:Int = 21
-    var age2:Int = 20
-    age2 = 21
-    println(age2)
+    var age2:Int = 22
 
     // Num more big -> Long (-9, 223, 372, 036, 854, 775, 807 a 9, 223, 372, 036, 854, 775, 807)
-    val example:Long = 21
+    val exampleLong:Long = 21
 
     // Float -> {five decimal places}.
     val floatExample:Float = 21.5f
 
     // Double -> {fourteen decimal places}.
     val doubleExample:Double = 21.14
+
+    //println("Add age + age2:")
+    //println(age + age2)
+
+    //println("Subtract age - age2:")
+    //println(age - age2)
+
+    //println("Multiply age * age2:")
+    //println(age * age2)
+
+    //println("Divide age / age2:")
+    //println(age / age2)
+
+    //println("Module age % age2:")
+    //println(age % age2)
+
+    var exampleAdd = age + floatExample.toInt()
+    //println(exampleAdd)
 
     /**
      * Alphanumeric variables.
@@ -34,7 +50,13 @@ fun main()
     val charExample2:Char = '#'
 
     // String -> {It is a garbage container and can withstand anything}.
-    val stringExample:String = "Hello world, my name is Leo"
+    val stringExample:String = "S0"
+    val stringExample1:String = "S1"
+
+    //println(stringExample + stringExample1) // Concatenation.
+    var stringConcatenation:String = "Hello"
+    stringConcatenation = "Hello, this is $stringExample1 and I have $age years old"
+    //println(stringConcatenation)
 
     /**
      * Boolean variables.
