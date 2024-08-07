de8889f0944b2527d23cc71980bc181529926c1e
Leonardo Zamudio Gómez <leoleozamudiogomez@gmail.com>
2024-08-07 05:08:31 -0600

    VariablesPart1

---
+++ app/src/main/java/com/leotesta017/androidstudiocourse/Variables.kt
@@ -0,0 +1,46 @@
+//Leonardo
+package com.leotesta017.androidstudiocourse
+
+// Variables
+
+fun main()
+{
+    /**
+     * Numerical Variables.
+     */
+
+    // Variable num INT (-2, 147, 483, 647 a 2, 147, 483, 647)
+    val age:Int = 21
+    var age2:Int = 20
+    age2 = 21
+    println(age2)
+
+    // Num more big -> Long (-9, 223, 372, 036, 854, 775, 807 a 9, 223, 372, 036, 854, 775, 807)
+    val example:Long = 21
+
+    // Float -> {five decimal places}.
+    val floatExample:Float = 21.5f
+
+    // Double -> {fourteen decimal places}.
+    val doubleExample:Double = 21.14
+
+    /**
+     * Alphanumeric variables.
+     */
+
+    // Char -> {It only supports one character}.
+    val charExample:Char = 'L'
+    val charExample1:Char = '9'
+    val charExample2:Char = '#'
+
+    // String -> {It is a garbage container and can withstand anything}.
+    val stringExample:String = "Hello world, my name is Leo"
+
+    /**
+     * Boolean variables.
+     */
+
+    //Boolean -> {True or false}.
+    val booleanExample:Boolean = true
+    val booleanExample1:Boolean = false
+}
